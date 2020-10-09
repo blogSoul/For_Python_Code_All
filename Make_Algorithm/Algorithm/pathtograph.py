6d53908eb36295f3c1628caf893d14ecaea81928
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-10-09 22:35:18 +0900

    Memo!

---
+++ Make_Algorithm/Algorithm/pathtograph.py
@@ -0,0 +1,54 @@
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+         
+def bfs(graph, start):
+    visited = []
+    queue = [start]
+
+    while queue:
+        n = queue.pop(0)
+        if n not in visited:
+            visited.append(n)
+            queue += graph[n] - set(visited)
+    return visited
+    
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    result = []
+
+    while queue:
+        n, path = queue.pop(0)
+        if n == goal:
+            result.append(path)
+        else:
+            for m in graph[n] - set(path):
+                queue.append((m, path + [m]))
+    return result
+    
+def dfs(graph, start):
+    visited = []
+    stack = [start]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack += graph[n] - set(visited)
+    return visited
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    result = []
+
+    while stack:
+        n, path = stack.pop()
+        if n == goal:
+            result.append(path)
+        else:
+            for m in graph[n] - set(path):
+                stack.append((m, path + [m]))
+    return result
